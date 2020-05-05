09d02947b1434591ef35dba035445417197af763
Rob Mathews <robbis@gmail.com>
2020-05-05 21:58:24 +0100

    add styling for App

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-	state = { videos: [] };
+	state = { videos: [], selectedVideo: null };
 	onTermSubmit = async (term) => {
 		const response = await youtube.get('/search', {
 			params: {
@@ -17,11 +18,27 @@ class App extends React.Component {
 		this.setState({ videos: response.data.items });
 	};
 
+	onVideoSelect = (video) => {
+		this.setState({ selectedVideo: video });
+	};
+
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
-				<VideoList videos={this.state.videos} />
+				<div className="ui grid">
+					<div className="ui row">
+						<div className="eleven wide column">
+							<VideoDetail video={this.state.selectedVideo} />
+						</div>
+						<div className="five wide column">
+							<VideoList
+								onVideoSelect={this.onVideoSelect}
+								videos={this.state.videos}
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
